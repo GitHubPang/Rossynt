30e13a38687f0dbd4c1a95a3795a9692f518f8fa
GitHubPang <61439577+GitHubPang@users.noreply.github.com>
2021-03-20 16:45:54 +0800

    Turn backend into web service.

--- backend/RoslynSyntaxTreeBackend/Program.cs
+++ backend/RoslynSyntaxTreeBackend/Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+#nullable enable
 
 namespace RoslynSyntaxTreeBackend {
-    internal static class Program {
-        private static async Task Main(string[] args) {
-            Console.WriteLine(string.Join(" ", args.Reverse()));
-            await Console.Out.FlushAsync();
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class Program {
+        public static void Main(string[] args) {
+            CreateHostBuilder(args).Build().Run();
         }
+
+        // ReSharper disable once MemberCanBePrivate.Global
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(builder => builder.AddSimpleConsole(options => options.SingleLine = true))
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
     }
 }
---
+++ backend/RoslynSyntaxTreeBackend/Startup.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+#nullable enable
+
+namespace RoslynSyntaxTreeBackend {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            app.UseRouting();
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+        }
+    }
+}
