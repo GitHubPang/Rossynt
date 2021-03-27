557816e1a4bbac54d0816cb0f13429ffb5e4b846
GitHubPang <61439577+GitHubPang@users.noreply.github.com>
2021-03-27 10:20:31 +0800

    Add back .NET Core 3.1 and .NET 5.0 to backend.

--- backend/RoslynSyntaxTreeBackend/Program.cs
+++ backend/RoslynSyntaxTreeBackend/Program.cs
@@ -1,17 +1,41 @@
-using System.Threading.Tasks;
+#if NET5_0
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+#elif NETCOREAPP3_1
+using Microsoft.Extensions.Hosting;
+#else
 using Microsoft.AspNetCore;
+#endif
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 
 namespace RoslynSyntaxTreeBackend {
     // ReSharper disable once ClassNeverInstantiated.Global
     public class Program {
         public static async Task Main(string[] args) {
+#if NETCOREAPP3_1 || NET5_0
+            await CreateHostBuilder(args).Build().RunAsync();
+#else
             await CreateWebHostBuilder(args).Build().RunAsync();
+#endif
         }
 
+#if NET5_0
+        // ReSharper disable once MemberCanBePrivate.Global
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(builder => builder.AddSimpleConsole(options => options.SingleLine = true))
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+#elif NETCOREAPP3_1
+        // ReSharper disable once MemberCanBePrivate.Global
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+#else
         // ReSharper disable once MemberCanBePrivate.Global
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+#endif
     }
 }
--- backend/RoslynSyntaxTreeBackend/Startup.cs
+++ backend/RoslynSyntaxTreeBackend/Startup.cs
@@ -13,12 +13,24 @@ namespace RoslynSyntaxTreeBackend {
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             services.AddSingleton<IProjectRepository, ProjectRepository>();
+#if NET5_0 || NETCOREAPP3_1
+            services.AddControllers();
+#else
             services.AddMvc();
+#endif
         }
 
+#if NET5_0 || NETCOREAPP3_1
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            app.UseRouting();
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+        }
+#else
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
             app.UseMvc(routes => routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}"));
         }
+#endif
     }
 }
