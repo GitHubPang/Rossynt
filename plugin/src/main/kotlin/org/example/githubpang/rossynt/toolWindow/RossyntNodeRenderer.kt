e008f477a2b0de0095a5063ae0297d189d59b85f
GitHubPang <61439577+GitHubPang@users.noreply.github.com>
2021-04-17 16:48:55 +0800

    Plugin: Show error in tree.

--- plugin/src/main/kotlin/org/example/githubpang/rossynt/toolWindow/RossyntNodeRenderer.kt
+++ plugin/src/main/kotlin/org/example/githubpang/rossynt/toolWindow/RossyntNodeRenderer.kt
@@ -1,12 +1,7 @@
 package org.example.githubpang.rossynt.toolWindow
 
-import com.intellij.icons.AllIcons
 import com.intellij.ide.util.treeView.NodeRenderer
-import com.intellij.util.ui.tree.TreeUtil
-import org.example.githubpang.rossynt.trees.TreeNode
-import org.example.githubpang.rossynt.trees.TreeNodeCategory
 import java.awt.Graphics2D
-import javax.swing.JTree
 
 /**
  * Modified from [com.intellij.ide.hierarchy.HierarchyNodeRenderer].
@@ -21,22 +16,5 @@ class RossyntNodeRenderer : NodeRenderer() {
     override fun doPaint(g: Graphics2D) {
         super.doPaint(g)
         isOpaque = false
-    }
-
-    override fun customizeCellRenderer(tree: JTree, value: Any, selected: Boolean, expanded: Boolean, leaf: Boolean, row: Int, hasFocus: Boolean) {
-        val treeNode = TreeUtil.getUserObject(TreeNode::class.java, value)
-        icon = if (treeNode != null) {
-            val nodeIcon = when (treeNode.treeNodeCategory) {
-                TreeNodeCategory.SyntaxNode -> AllIcons.Nodes.Folder
-                TreeNodeCategory.SyntaxToken -> AllIcons.Actions.Words
-                TreeNodeCategory.LeadingTrivia -> AllIcons.Actions.InlayRenameInComments
-                TreeNodeCategory.TrailingTrivia -> AllIcons.Actions.InlayRenameInComments
-            }
-            fixIconIfNeeded(nodeIcon, selected, hasFocus)
-        } else {
-            null
-        }
-
-        super.customizeCellRenderer(tree, value, selected, expanded, leaf, row, hasFocus)
     }
 }
--- plugin/src/main/kotlin/org/example/githubpang/rossynt/trees/TreeNode.kt
+++ plugin/src/main/kotlin/org/example/githubpang/rossynt/trees/TreeNode.kt
@@ -2,6 +2,11 @@ package org.example.githubpang.rossynt.trees
 
 import com.google.common.collect.ImmutableList
 import com.google.gson.annotations.SerializedName
+import com.intellij.icons.AllIcons
+import com.intellij.ide.projectView.PresentationData
+import com.intellij.navigation.ItemPresentation
+import com.intellij.navigation.NavigationItem
+import com.intellij.openapi.editor.colors.CodeInsightColors
 
 internal data class TreeNode(
     @SerializedName("Id") val nodeId: String,
@@ -9,8 +14,14 @@ internal data class TreeNode(
     @SerializedName("Type") val rawType: String,
     @SerializedName("Kind") val syntaxKind: String,
     @SerializedName("Str") val shortString: String,
+    @SerializedName("IsMissing") private val isMissingInt: Int,
     @SerializedName("Child") private val childTreeNodes: List<TreeNode>?
-) {
+) : NavigationItem {
+    private val isMissing: Boolean
+        get() {
+            return isMissingInt == 1
+        }
+
     fun childTreeNodes(): ImmutableList<TreeNode> {
         return if (childTreeNodes != null) {
             ImmutableList.copyOf(childTreeNodes)
@@ -18,9 +29,30 @@ internal data class TreeNode(
             ImmutableList.of()
     }
 
-    //todo better not override toString?
-    override fun toString(): String {
-        //todo what about line breaks? white spaces?
-        return syntaxKind + " " + shortString
+    override fun navigate(requestFocus: Boolean): Unit = Unit
+    override fun canNavigate(): Boolean = false
+    override fun canNavigateToSource(): Boolean = false
+    override fun getName(): String? = null
+    override fun getPresentation(): ItemPresentation = PresentationData(
+        syntaxKind,
+        shortString, //todo what about line breaks? white spaces? null characters?
+        when (treeNodeCategory) {
+            TreeNodeCategory.SyntaxNode -> AllIcons.Nodes.Folder
+            TreeNodeCategory.SyntaxToken -> AllIcons.Actions.Words
+            TreeNodeCategory.LeadingTrivia -> AllIcons.Actions.InlayRenameInComments
+            TreeNodeCategory.TrailingTrivia -> AllIcons.Actions.InlayRenameInComments
+        },
+        if (isError()) CodeInsightColors.ERRORS_ATTRIBUTES else null
+    )
+
+    /**
+     * @see <a href="https://docs.microsoft.com/en-us/dotnet/csharp/roslyn-sdk/work-with-syntax#errors">Microsoft Docs | Use the .NET Compiler Platform SDK syntax model | Errors</a>
+     */
+    private fun isError(): Boolean {
+        if (isMissing || syntaxKind == "SkippedTokensTrivia") {
+            return true
+        }
+
+        return childTreeNodes != null && childTreeNodes.any { it.isError() }
     }
 }
