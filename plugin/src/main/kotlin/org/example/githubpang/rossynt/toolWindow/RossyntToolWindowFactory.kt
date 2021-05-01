323f21461937db03306562074d5034a98ee78ea7
GitHubPang <61439577+GitHubPang@users.noreply.github.com>
2021-05-01 18:19:47 +0800

    Plugin: Make tool window content show up without waiting for indexing to complete.

--- plugin/src/main/kotlin/org/example/githubpang/rossynt/toolWindow/RossyntToolWindowFactory.kt
+++ plugin/src/main/kotlin/org/example/githubpang/rossynt/toolWindow/RossyntToolWindowFactory.kt
@@ -1,11 +1,12 @@
 package org.example.githubpang.rossynt.toolWindow
 
+import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.ContentFactory
 
-internal class RossyntToolWindowFactory : ToolWindowFactory {
+internal class RossyntToolWindowFactory : ToolWindowFactory, DumbAware {
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         require(toolWindow.id == RossyntToolWindow.TOOL_WINDOW_ID)
 
