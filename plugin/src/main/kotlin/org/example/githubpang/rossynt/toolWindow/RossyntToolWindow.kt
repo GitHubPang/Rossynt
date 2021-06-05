6f3427883a2f6ffed1990c9ba7f7ee632977e33b
GitHubPang <61439577+GitHubPang@users.noreply.github.com>
2021-06-05 16:20:40 +0800

    Revert commit 268709a5237b57ab10a7579d4ad80b86995085cc concerning CollapseAllAction.

--- plugin/src/main/kotlin/org/example/githubpang/rossynt/toolWindow/RossyntToolWindow.kt
+++ plugin/src/main/kotlin/org/example/githubpang/rossynt/toolWindow/RossyntToolWindow.kt
@@ -4,9 +4,7 @@ import com.google.common.collect.ImmutableList
 import com.google.common.collect.ImmutableMap
 import com.intellij.CommonBundle
 import com.intellij.icons.AllIcons
-import com.intellij.ide.DefaultTreeExpander
 import com.intellij.ide.HelpTooltip
-import com.intellij.ide.actions.CollapseAllAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.ToggleAction
 import com.intellij.openapi.components.service
@@ -25,6 +23,7 @@ import com.intellij.ui.EditorNotificationPanel
 import com.intellij.ui.JBSplitter
 import com.intellij.ui.ScrollPaneFactory
 import com.intellij.ui.treeStructure.Tree
+import com.intellij.ui.treeStructure.actions.CollapseAllAction
 import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.StatusText
 import com.intellij.util.ui.tree.TreeUtil
@@ -136,7 +135,6 @@ internal class RossyntToolWindow(private val project: Project, toolWindow: ToolW
 
     // UI.
     private val uiTree: Tree = Tree()
-    private val myTreeExpander = DefaultTreeExpander(uiTree)
     private val uiBanner = EditorNotificationPanel(MessageType.ERROR.popupBackground)
     private val uiTable: JTable = JTable(UiTableModel())
     private val uiSplitter: JBSplitter = JBSplitter()
@@ -164,7 +162,7 @@ internal class RossyntToolWindow(private val project: Project, toolWindow: ToolW
         })
 
         // Add tool window buttons.
-        val collapseAction = CollapseAllAction { myTreeExpander }
+        val collapseAction = CollapseAllAction(uiTree)
         collapseAction.templatePresentation.icon = AllIcons.Actions.Collapseall
         toolWindow.setTitleActions(listOf(ToggleHighlightNodeInSourceAction(), collapseAction))
 
