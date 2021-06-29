b31c18059c6756c48e1962c5cd4319be0df6a32f
GitHubPang <61439577+GitHubPang@users.noreply.github.com>
2021-06-29 08:36:39 +0800

    Update plugin dependencies.
    https://kotlinlang.org/docs/releases.html#release-details

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -28,10 +28,10 @@ repositories {
 }
 dependencies {
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.17.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.4.2-native-mt")
-    implementation("io.ktor:ktor-client-core:1.5.1")
-    implementation("io.ktor:ktor-client-cio:1.5.1")
-    implementation("io.ktor:ktor-client-gson:1.5.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.5.0-native-mt")
+    implementation("io.ktor:ktor-client-core:1.5.4")
+    implementation("io.ktor:ktor-client-cio:1.5.4")
+    implementation("io.ktor:ktor-client-gson:1.5.4")
 }
 
 // Configure gradle-intellij-plugin plugin.
