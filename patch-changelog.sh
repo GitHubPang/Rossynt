228f1a8c6c91797ca564a7b1dad07c75c31e4694
GitHubPang <61439577+GitHubPang@users.noreply.github.com>
2021-07-07 08:42:45 +0800

    Work on patch changelog script.

--- patch-changelog.sh
+++ patch-changelog.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+head CHANGELOG.md
+
 PLUGIN_VERSION=$(grep -E '^pluginVersion\s*=\s*.*$' plugin/gradle.properties)
 PLUGIN_VERSION=${PLUGIN_VERSION##* }
 echo Plugin version is \'${PLUGIN_VERSION}\'.
@@ -8,3 +10,5 @@ if [ -z "${PLUGIN_VERSION}" ]; then
 fi
 
 sed -i -E "s/\[Unreleased\]/\[Unreleased\]\n## \[${PLUGIN_VERSION}\] - $(date --iso-8601)/g" CHANGELOG.md || exit $?
+
+head CHANGELOG.md
