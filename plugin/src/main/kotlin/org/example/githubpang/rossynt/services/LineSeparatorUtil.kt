2cfee2b8216c1f755954a3ed2c15fc296bd47175
GitHubPang <61439577+GitHubPang@users.noreply.github.com>
2021-08-20 08:13:05 +0800

    Mark convertLineSeparators() as pure.

--- plugin/src/main/kotlin/org/example/githubpang/rossynt/services/LineSeparatorUtil.kt
+++ plugin/src/main/kotlin/org/example/githubpang/rossynt/services/LineSeparatorUtil.kt
@@ -1,8 +1,10 @@
 package org.example.githubpang.rossynt.services
 
 import com.intellij.openapi.util.text.StringUtil
+import org.jetbrains.annotations.Contract
 
 internal object LineSeparatorUtil {
+    @Contract(pure = true)
     fun convertLineSeparators(text: String?, newSeparator: String): String? = when (text) {
         null -> null
         else -> StringUtil.convertLineSeparators(text, newSeparator)
