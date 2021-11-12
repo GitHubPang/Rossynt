d1b22fa3f043bb1e073096e871fbcacd64ba92ef
GitHubPang <61439577+GitHubPang@users.noreply.github.com>
2021-11-12 08:40:28 +0800

    Add test case for C# 10 file-scoped namespace declaration.

--- backend/RossyntBackendIntegrationTest/IntegrationTest.cs
+++ backend/RossyntBackendIntegrationTest/IntegrationTest.cs
@@ -40,11 +40,18 @@ namespace RossyntBackendIntegrationTest {
         });
 
         [Test]
-        public Task CompileFileCSharp10() => RunWithHttpClient(async httpClient => {
+        public Task CompileFileCSharp10GlobalUsingDirectives() => RunWithHttpClient(async httpClient => {
             var root = await CompileFile(httpClient, "global using System;");
             AssertNode(root, "SyntaxNode", "CompilationUnitSyntax", "CompilationUnit", "global using System;", "0,20", false, 2);
             AssertNode(root["Child"]?[0], "SyntaxNode", "UsingDirectiveSyntax", "UsingDirective", "global using System;", "0,20", false, 4);
             AssertNode(root["Child"]?[0]?["Child"]?[0], "SyntaxToken", "SyntaxToken", "GlobalKeyword", "global", "0,6", false, 1);
+        });
+
+        [Test]
+        public Task CompileFileCSharp10FileScopedNamespaceDeclaration() => RunWithHttpClient(async httpClient => {
+            var root = await CompileFile(httpClient, "namespace MyNamespace;");
+            AssertNode(root, "SyntaxNode", "CompilationUnitSyntax", "CompilationUnit", "namespace MyNamespace;", "0,22", false, 2);
+            AssertNode(root["Child"]?[0], "SyntaxNode", "FileScopedNamespaceDeclarationSyntax", "FileScopedNamespaceDeclaration", "namespace MyNamespace;", "0,22", false, 3);
         });
 
         [Test]
