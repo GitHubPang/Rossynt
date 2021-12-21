0dcc187bc41d1d8fe574ba2d04bf82911561bea1
GitHubPang <61439577+GitHubPang@users.noreply.github.com>
2021-12-21 08:28:08 +0800

    Optimize imports.

--- backend/RossyntBackend/ApplicationLifetime/ApplicationLifetimeService.cs
+++ backend/RossyntBackend/ApplicationLifetime/ApplicationLifetimeService.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 #nullable enable
 
