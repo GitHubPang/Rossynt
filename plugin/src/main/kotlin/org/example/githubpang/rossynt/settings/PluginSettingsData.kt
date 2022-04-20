d5314eb0b7c5d4e400ba9d8afc5076ffe4df4c3d
GitHubPang <61439577+GitHubPang@users.noreply.github.com>
2022-04-20 08:55:33 +0800

    Remove usages of GlobalScope in PluginSettingsData.
    Reference: https://plugins.jetbrains.com/docs/intellij/general-threading-rules.html#modality-and-invokelater

--- plugin/src/main/kotlin/org/example/githubpang/rossynt/settings/PluginSettingsData.kt
+++ plugin/src/main/kotlin/org/example/githubpang/rossynt/settings/PluginSettingsData.kt
@@ -3,9 +3,6 @@ package org.example.githubpang.rossynt.settings
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.*
 import com.intellij.util.xmlb.XmlSerializerUtil
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 @Service
 @State(name = "org.example.githubpang.rossynt.settings.PluginSettingsData", storages = [Storage("Rossynt.PluginSettingsData.xml", roamingType = RoamingType.DISABLED)])
@@ -22,7 +19,7 @@ internal class PluginSettingsData : PersistentStateComponent<PluginSettingsData?
             }
             field = value
 
-            GlobalScope.launch(Dispatchers.Main) {
+            ApplicationManager.getApplication().invokeLater {
                 if (this@PluginSettingsData == instance) {
                     // Publish message.
                     val messageBus = ApplicationManager.getApplication()?.messageBus
