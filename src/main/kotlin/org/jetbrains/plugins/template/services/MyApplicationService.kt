0233816935649fd055698c5bd19a0dbae070c607
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2022-06-15 11:34:10 +0200

    code cleanup
    Cherry-picked from commit 5915fcbb64b6bb02406da0290550f9db54e12b17

--- src/main/kotlin/org/jetbrains/plugins/template/services/MyApplicationService.kt
+++ src/main/kotlin/org/jetbrains/plugins/template/services/MyApplicationService.kt
@@ -6,5 +6,8 @@ class MyApplicationService {
 
     init {
         println(MyBundle.message("applicationService"))
+
+        System.getenv("CI")
+            ?: TODO("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
     }
 }
