0de1790404b50586ddf0c59dc95686995e6081ba
GitHubPang <61439577+GitHubPang@users.noreply.github.com>
2023-01-20 08:48:45 +0800

    Migrate away from deprecated API.
    ProjectManagerListener.projectOpened(Project) is deprecated and scheduled for removal.
    Migrate to com.intellij.openapi.startup.StartupActivity.

--- plugin/src/main/kotlin/org/example/githubpang/rossynt/startup/AppStartupActivity.kt
+++ plugin/src/main/kotlin/org/example/githubpang/rossynt/startup/AppStartupActivity.kt
@@ -1,14 +1,12 @@
-package org.example.githubpang.rossynt.listeners
+package org.example.githubpang.rossynt.startup
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.project.ProjectManagerListener
+import com.intellij.openapi.startup.StartupActivity
 import org.example.githubpang.rossynt.services.RossyntService
 
-internal class AppProjectManagerListener : ProjectManagerListener {
-    override fun projectOpened(project: Project) {
-        super.projectOpened(project)
-
+internal class AppStartupActivity : StartupActivity {
+    override fun runActivity(project: Project) {
         // Start Rossynt service if needed.
         val rossyntService = project.service<RossyntService>()
         rossyntService.startRossyntServiceIfNeeded(project)
