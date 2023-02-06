a8baf7aa2e21ad9963b5800e155529b23a013a62
GitHubPang <61439577+GitHubPang@users.noreply.github.com>
2023-02-06 09:15:48 +0800

    In plugin, drop support for .NET 5.

--- plugin/src/main/kotlin/org/example/githubpang/rossynt/services/BackendRuntimeVersion.kt
+++ plugin/src/main/kotlin/org/example/githubpang/rossynt/services/BackendRuntimeVersion.kt
@@ -1,7 +1,6 @@
 package org.example.githubpang.rossynt.services
 
 internal enum class BackendRuntimeVersion(val majorVersion: Int, val directoryName: String) {
-    DOT_NET_5(5, "net5.0"),
     DOT_NET_6(6, "net6.0"),
     DOT_NET_7(7, "net7.0"),
 }
--- plugin/src/test/kotlin/org/example/githubpang/rossynt/services/BackendProcessOutputParserTest.kt
+++ plugin/src/test/kotlin/org/example/githubpang/rossynt/services/BackendProcessOutputParserTest.kt
@@ -12,13 +12,6 @@ class BackendProcessOutputParserTest internal constructor(private val line: Stri
         @Parameterized.Parameters
         fun data(): Array<Any> {
             return arrayOf(
-                // .NET 5.0
-                arrayOf("""info: Microsoft.Hosting.Lifetime[0] Now listening on: https://localhost:5001""", "https", "5001"),
-                arrayOf("""info: Microsoft.Hosting.Lifetime[0] Now listening on: http://localhost:5000""", "http", "5000"),
-                arrayOf("""info: Microsoft.Hosting.Lifetime[0] Application started. Press Ctrl+C to shut down.""", null, null),
-                arrayOf("""info: Microsoft.Hosting.Lifetime[0] Hosting environment: Development""", null, null),
-                arrayOf("""info: Microsoft.Hosting.Lifetime[0] Content root path: C:\Rossynt\backend\RossyntBackend""", null, null),
-
                 // .NET 6.0
                 arrayOf("""info: Microsoft.Hosting.Lifetime[14] Now listening on: https://localhost:5001""", "https", "5001"),
                 arrayOf("""info: Microsoft.Hosting.Lifetime[14] Now listening on: http://localhost:5000""", "http", "5000"),
